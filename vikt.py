5f08f3a6cb42bded42159ca9a465b13a93e4fd54
Shhamann <114716744+Shhamann@users.noreply.github.com>
2022-12-03 10:11:00 +0700

    Add files via upload

---
+++ vikt.py
@@ -0,0 +1,12 @@
+def vikt():
+    que = ['1000 - 7 = ?', 'Висит груша, нельзя скушать', '']
+    ans = ['993', 'Лампочка']
+    kolvo = 0
+    for i  in range(2):
+        print(que[i])
+        yans = str(input("Ответ:"))
+        if yans == ans[i]:
+            kolvo += 1
+    print(f'Количество правильных ответов:{kolvo}')
+    # return str(input(f' начать с начала?{x}'))
+vikt()
