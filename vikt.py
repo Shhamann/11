96e6ed135725c87f374ec83b4ba299f8537ce5e7
Shhamann <114716744+Shhamann@users.noreply.github.com>
2022-12-17 10:05:38 +0700

    Update vikt.py

--- vikt.py
+++ vikt.py
@@ -1,12 +1,15 @@
-def vikt():
-    que = ['1000 - 7 = ?', 'Висит груша, нельзя скушать', '']
-    ans = ['993', 'Лампочка']
+def vikt(x):
+    que = ['1000 - 7 = ?', 'Висит груша, нельзя скушать', 'Зимой и летом одним цветом', '3^2 = ', '2 + 3 = ']
+    ans = ['993', 'лампочка', 'ёлка', '9', '5']
     kolvo = 0
-    for i  in range(2):
+    for i  in range(5):
         print(que[i])
         yans = str(input("Ответ:"))
         if yans == ans[i]:
             kolvo += 1
     print(f'Количество правильных ответов:{kolvo}')
-    # return str(input(f' начать с начала?(yes/no){x}'))
-vikt()
+    return str(input(f' начать с начала?(yes/no)'))
+a = str('yes')
+while a == 'yes':
+    x = str(a)
+    a = vikt(x)
