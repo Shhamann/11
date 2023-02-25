60afd1609c197ef219c780ea2cefab1c77e9fed9
Shhamann <114716744+Shhamann@users.noreply.github.com>
2023-02-25 13:04:24 +0700

    Update 24.py

--- 2polygodie/решение задач/24.py
+++ 2polygodie/решение задач/24.py
@@ -1,19 +1,19 @@
 with open('24.txt') as f:
    let=f.readline()
-   count,maximum=0,0
+   c,maxi=0,0
    #text=''
-   delta=0
+   delt=0
    for i in range(len(let)-1):
       if (let[i]=='C' or let[i]=='D' or let[i]=='F') and (let[i+1]=='A' or let[i+1]=='O'):
-         delta=0
+         delt=0
          #text=text+let[i]+let[i+1]
-         count+=1
-         maximum=max(count,maximum)
-      else: delta+=1
-      if delta==2:
-         count=0
+         c+=1
+         maxi=max(c,max)
+      else: delt+=1
+      if delt==2:
+         c=0
          #text=''
-print(maximum)
+print(maxi)
 
 with open('24.txt') as f:
     s=f.readline().replace('C','S').replace('D','S').replace('F','S')
