92c09b8d795d6920ae6d5abc3f98289d34b3b153
Shhamann <114716744+Shhamann@users.noreply.github.com>
2023-04-01 13:29:45 +0700

    Update sort.py

--- 2polygodie/sort.py
+++ 2polygodie/sort.py
@@ -1,14 +1,13 @@
-s = [1, 0, 10, 2, 4]
-while True:
-    st = input()
-    if st == "stop":
-        break
+s = [100, 0, 1000, 10, 2, 4]
+a = 1
+while a == 1:
+    if all(s[i] <= s[i + 1] for i in range(len(s)-1)):
+        a = 2
     for i in range(len(s)-1):
         if(s[i] > s[i+1]):
             s[i], s[i+1] = s[i+1], s[i]
     for i in range(len(s)):
         print(s[i])
-     
-    
+  
         
     
