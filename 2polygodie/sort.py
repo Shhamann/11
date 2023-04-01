e1ba057482821fa4268461e73526b33bccc79a93
Shhamann <114716744+Shhamann@users.noreply.github.com>
2023-04-01 13:53:55 +0700

    Update sort.py

--- 2polygodie/sort.py
+++ 2polygodie/sort.py
@@ -1,13 +1,13 @@
 s = [100, 0, 1000, 10, 2, 4]
 a = 1
 while a == 1:
-    if all(s[i] <= s[i + 1] for i in range(len(s)-1)):
-        a = 2
-    for i in range(len(s)-1):
-        if(s[i] > s[i+1]):
+    
+    for i in range(len(s)-1): 
+        if(s[i] > s[i+1]): #если текущее число больше следующего, то меняем их местами
             s[i], s[i+1] = s[i+1], s[i]
     for i in range(len(s)):
         print(s[i])
-  
-        
+    if all(s[i] <= s[i + 1] for i in range(len(s)-1)): #проверка(все ли числа в порядке возраствния?)
+        a = 2
+     
     
